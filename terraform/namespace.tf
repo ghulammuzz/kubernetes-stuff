735746f24e55673cf690c218b19b28355b02b89a
ghulammuzz <ghulamxiaomi@gmail.com>
2024-10-27 03:26:09 +0700

    del terraform, to much :v
    Signed-off-by: ghulammuzz <ghulamxiaomi@gmail.com>

--- terraform/namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace" "muzz" {
-  metadata {
-    name = "muzz"
-  }
-}
--- terraform/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "long-pond-86927214"
-}
